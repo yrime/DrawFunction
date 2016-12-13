588ecc2761cb7f445a94bef4dfc03f305f01bdff
yrime <yrime@DESKTOP-V6GQ6B3>
2016-12-13 13:42:33 +0300

    last version added more graphics

--- DrawFunction/GraphicFrame.Designer.cs
+++ DrawFunction/GraphicFrame.Designer.cs
@@ -37,6 +37,9 @@
             this.zumUp = new System.Windows.Forms.Button();
             this.zumDown = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -93,24 +96,58 @@
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Left;
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(28, 12);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(970, 620);
+            this.chart1.Size = new System.Drawing.Size(998, 644);
             this.chart1.TabIndex = 7;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1004, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 59);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1004, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 63);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "time";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1004, 425);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 63);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // GraphicFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1178, 644);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.zumDown);
             this.Controls.Add(this.zumUp);
@@ -133,6 +170,9 @@
         private System.Windows.Forms.Button zumUp;
         private System.Windows.Forms.Button zumDown;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- DrawFunction/GraphicFrame.cs
+++ DrawFunction/GraphicFrame.cs
@@ -4,13 +4,18 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml.Serialization;
 
 namespace DrawFunction
 {
     public partial class GraphicFrame : Form
     {
-        double zum = 10;
+        bool color = true;
+
+        int maxPoints = 60;
+        int series = -1;
+        double zum = 50;
         FileStream fs;
         StreamReader rw;
         XmlSerializer serializator = new XmlSerializer(typeof(List<double>));
@@ -18,17 +23,49 @@ namespace DrawFunction
         public GraphicFrame()
         {
             InitializeComponent();
+            chart1.ChartAreas.Add(new ChartArea("mychart"));
+            chart1.ChartAreas["mychart"].InnerPlotPosition = new ElementPosition(5,-70,100,100);
         }
 
         private void draw_Click(object sender, EventArgs e)
         {
-            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
-            chart1.Series[0].Points.Clear();
-            List<double> acsXY = this.getList();
-            for(int i = 0; i < acsXY.Count(); ++i)
+            double point = 0;
+            int listPoint;
+            double timebuf = 0;
+            ++series;
+            chart1.Series.Add(new Series(series.ToString()));
+            chart1.Series[series.ToString()].ChartArea = "mychart";
+            chart1.Series[series.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            if (color)
             {
-                chart1.Series[0].Points.AddXY(i, acsXY[i]);
+                chart1.Series[series.ToString()].Color = Color.Red;
+            }else
+            {
+                chart1.Series[series.ToString()].Color = Color.Blue;
             }
+            chart1.Series[series.ToString()].BorderWidth = 1;
+            List<double> acsXY = this.getList();
+            List<double> acsTime = this.getTime();
+            listPoint = acsXY.Count();
+            for(int i = 0; i < listPoint; ++i)
+            {
+                //  point += ((double)maxPoints / (double)listPoint);
+                point = i;
+                chart1.Series[series.ToString()].Points.AddXY((acsTime[i]), acsXY[i]);
+            }
+           // chart1.Series[0].Points.
+        }
+
+        private List<double> getTime()
+        {
+            List <double> lout;
+            XmlSerializer serializator = new XmlSerializer(typeof(List<double>));
+            fs = File.Open(button2.Text.ToString(), FileMode.Open);
+            rw = new StreamReader(fs);
+            lout = (List<double>)serializator.Deserialize(rw);
+            rw.Close();
+            fs.Close();
+            return lout;
         }
 
         private List<double> getList()
@@ -49,28 +86,49 @@ namespace DrawFunction
             {
                 fileName.Text = openFileDialog.FileName;
             }
+            
         }
 
         private void zumUp_Click(object sender, EventArgs e)
         {
-            zum += 0.002;
-            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
-            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(zum, zum + 0.002);
-            chart1.ChartAreas[0].AxisY.ScaleView.Zoom(zum, zum + 0.002);
+            //chart1.Series[series.ToString()]
+            zum -= 5;
+            chart1.ChartAreas["mychart"].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas["mychart"].AxisX.ScaleView.Zoom(0, zum);
+            chart1.ChartAreas["mychart"].AxisY.ScaleView.Zoom(0, zum);
         }
 
         private void zumDown_Click(object sender, EventArgs e)
         {
-            zum = 1;
-            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
-
-            chart1.ChartAreas[0].AxisX.ScaleView.ZoomReset();
-            chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+            zum += 5;
+            chart1.ChartAreas["mychart"].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas["mychart"].AxisX.ScaleView.Zoom(0, zum);
+            chart1.ChartAreas["mychart"].AxisY.ScaleView.Zoom(0, zum);
         }
 
         private void chart1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            series = -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                button2.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.color = !this.color;
         }
     }
 }
